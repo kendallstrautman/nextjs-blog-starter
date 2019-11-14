66483b296c0285dc5e4d363b146b722d7b1d6bcb
Kendall Strautman <kendallstrautman@gmail.com>
2019-11-14 12:52:59 -0800

    initial commit

---
+++ src/components/BlogList.js
@@ -0,0 +1,21 @@
+const BlogList = () => {
+
+  return (
+    <div>
+      <h1>List of all blog posts will go here</h1>
+      <style jsx>
+        {`
+          div {
+            width: 100%;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default BlogList;
---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Layout from "../components/Layout";
+import BlogList from "../components/BlogList";
+
+const Index = (props) => {
+  return (
+    <Layout pathname="/" siteTitle={props.title}>
+      <section>
+        <BlogList />
+      </section>
+    </Layout>
+  );
+};
+
+export default Index;
+
+Index.getInitialProps = async function() {
+  const content = await import(`../data/config.json`)
+
+  return {
+    fileRelativePath: `src/data/config.json`,
+    ...content
+  }
+}
