8aa82e3a666fb5e64675302a57ba35844b21823b
Kendall Strautman <kendallstrautman@gmail.com>
2020-04-01 14:04:19 -0600

    chore: index uses getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,28 @@
-import Layout from "../components/Layout";
-import BlogList from "../components/BlogList";
+import Layout from '../components/Layout'
+import BlogList from '../components/BlogList'
 
-const Index = (props) => {
+const Index = props => {
   return (
-    <Layout 
-      pathname="/" 
-      siteTitle={props.title} 
+    <Layout
+      pathname="/"
+      siteTitle={props.title}
       siteDescription={props.description}
     >
       <section>
         <BlogList />
       </section>
     </Layout>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
 
-Index.getInitialProps = async function() {
+export async function getStaticProps() {
   const configData = await import(`../data/config.json`)
   return {
-    ...configData
+    props: {
+      title: configData.title,
+      description: configData.description,
+    },
   }
 }
